3b942e1d5e9065101fa5984d90b66a010121d765
arduomeng <arduomeng@admindeMac-mini-2.local>
2016-12-14 17:56:15 +0800

    xx

---
+++ 国际化/国际化/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  国际化
+//
+//  Created by arduomeng on 16/12/14.
+//  Copyright © 2016年 arduomeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 国际化/国际化/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  国际化
+//
+//  Created by arduomeng on 16/12/14.
+//  Copyright © 2016年 arduomeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
